88b0560d0254be9c954d272d1ed74d16405d84b8
lubhansh01 <sanskrtisharma999@gmail.com>
2026-05-22 12:06:39 +0530

    commit change

--- Q1.Grocery shop problem.py
+++ Q1.Grocery shop problem.py
@@ -1,13 +1,21 @@
 #A grocery shop owner wants to calculate the final bill after applying discounts based on the purchase amount. Write a program using if...elif...else to display the final amount.
-print("Enter the purchase amount :")
-amount = float(input())
-if amount > 500:
-    discount = 0
-elif amount <= 500 and amount > 1000:
-    discount = 0.5/100 * amount
-elif amount <= 1000 and amount > 5000:
-    discount = 1.0/100 * amount
-else : discount = 1.5/100 * amount
-final_amount  = amount - (amount  * discount / 100)
 
-print("The final amount after discount is : ", final_amount)
+amount = float(input("Enter purchase amount: "))
+
+if amount >= 5000:
+    discount = amount * 0.20  
+elif amount >= 3000:
+    discount = amount * 0.15   
+elif amount >= 1000:
+    discount = amount * 0.10  
+else:
+    discount = 0               
+
+final_amount = amount - discount
+
+print("Discount:", discount)
+print("Final Bill Amount:", final_amount)
+
+#Enter purchase amount: 7500
+#Discount: 1500.0
+#Final Bill Amount: 6000.0
---
+++ Q2.Employee eligiblty.py
@@ -0,0 +1,3 @@
+# A company wants to identify whether an employee is eligible for a bonus based on attendance percentage andperformance rating.
+
+print("Enter the attendence percentage :")
